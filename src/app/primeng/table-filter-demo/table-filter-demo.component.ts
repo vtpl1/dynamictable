053b7e55784c34c512d974966559968659e88b51
Monotosh Das <monotosh.das@videonetics.com>
2020-11-26 07:11:48 +0000

    test

--- src/app/primeng/table-filter-demo/table-filter-demo.component.ts
+++ src/app/primeng/table-filter-demo/table-filter-demo.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Customer, Representative } from '../domain/customer';
 import { CustomerService } from '../service/customer-service.service';
+import jsPDF from 'jspdf';
+import autoTable from 'jspdf-autotable';
 
 @Component({
   selector: 'app-table-filter-demo',
@@ -9,6 +11,30 @@ import { CustomerService } from '../service/customer-service.service';
 })
 export class TableFilterDemo implements OnInit {
   customers: Customer[];
+
+  head1 = [
+    [
+      'Name',
+      'Country',
+      'Agent',
+      'Date',
+      'Balance',
+      'Status',
+      'Activity',
+      'Verfied',
+    ],
+  ];
+  customers1: [[1, 'IT', 2, 1, 0, 1, 7, 8], [2, 'COMP', 2, 1, 0, 1, 7, 8]];
+  head = [['ID', 'Country', 'Index', 'Capital']];
+  data = [
+    [1, 'Finland', 7.632, 'Helsinki'],
+    [2, 'Norway', 7.594, 'Oslo'],
+    [3, 'Denmark', 7.555, 'Copenhagen'],
+    [4, 'Iceland', 7.495, 'Reykjavík'],
+    [5, 'Switzerland', 7.487, 'Bern'],
+    [9, 'Sweden', 7.314, 'Stockholm'],
+    [73, 'Belarus', 5.483, 'Minsk'],
+  ];
 
   representatives: Representative[];
 
@@ -35,17 +61,19 @@ export class TableFilterDemo implements OnInit {
     });
 
     this.cols = [
-      { field: 'name', header : 'Name'},
-      { field: 'country', header : 'Country'},
-      { field: 'agent', header : 'Agent'},
-      { field: 'date', header : 'Date'},
-      { field: 'balance', header : 'Balance'},
-      { field: 'status', header : 'Status'},
-      { field: 'activity', header : 'Activity'},
-      { field: 'verified', header : 'Verified'},
-
+      { field: 'name', header: 'Name' },
+      { field: 'country', header: 'Country' },
+      { field: 'agent', header: 'Agent' },
+      { field: 'date', header: 'Date' },
+      { field: 'balance', header: 'Balance' },
+      { field: 'status', header: 'Status' },
+      { field: 'activity', header: 'Activity' },
+      { field: 'verified', header: 'Verified' },
     ];
-    this.exportColumns = this.cols.map(col => ({title: col.header, dataKey: col.field}));
+    this.exportColumns = this.cols.map((col) => ({
+      title: col.header,
+      dataKey: col.field,
+    }));
     this.representatives = [
       { name: 'Amy Elsner', image: 'amyelsner.png' },
       { name: 'Anna Fali', image: 'annafali.png' },
@@ -69,13 +97,24 @@ export class TableFilterDemo implements OnInit {
     ];
   }
   exportPdf() {
-    import('jspdf').then((jsPDF) => {
-      import('jspdf-autotable').then((x) => {
-        const doc = new jsPDF.default(0, 0);
-        doc.autoTable(this.exportColumns, this.customers);
-        doc.save('products.pdf');
-      });
+    const doc = new jsPDF('portrait');
+    autoTable(doc, {
+      bodyStyles: {minCellHeight: 25},
+      head: this.head1,
+      body: this.data,
+
+      didDrawCell: (data) => {
+        if (data.column.index === 5 && data.cell.section === 'body') {
+          let td = data.cell.raw;
+          let dim = data.cell.height - data.cell.padding('vertical');
+          let textPos = data.cell.getTextPos();
+          let img_url = 'assets/images/no_image_available.png';
+          doc.addImage(img_url, textPos.x, textPos.y, dim, dim);
+          console.log(dim)
+        }
+      },
     });
+    doc.save('products.pdf');
   }
   exportExcel() {
     import('xlsx').then((xlsx) => {
